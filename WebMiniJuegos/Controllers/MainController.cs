15b59f69792e26bd0d953d8ae1e309fdfab314b2
Alexandro <alejandro.gh98@outlook.com>
2024-06-30 00:19:02 -0600

    Error css en index.html padding 25

---
+++ WebMiniJuegos/Controllers/MainController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebMiniJuegos.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Quiz()
+        {
+            return View("DwarfQuiz");
+        }
+
+        public ActionResult Nightmare()
+        {
+            return View("Dungeon");
+        }
+
+        public ActionResult PokeBattle()
+        {
+            return View("PokemonBattle");
+        }
+    }
+}
